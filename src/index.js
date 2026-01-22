89f909b9a6e7c1370575e263019e1fdf5baf2cfa
bhavishtrehans135 <bhavish.trehan.s.135@kalvium.community>
2026-01-22 10:38:19 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const http=require('http');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'content-type':'text/plain'})
-    resp.end("hello world");
-}).listen(7600);
+const express=require('express')
+const postsRoutes=require('./routes/post.route.js')
+const app=express()
+
+app.get("/",(req,resp)=>{
+    resp.status(200).send("welcome to blogify")
+})
+
+app.use("/api/v1/posts",postsRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
